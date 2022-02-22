b342abcd70be84eef0f70bfb8aab7ca87880903f
mehmedh <mhofficial949@gmail.com>
2022-02-22 23:07:18 +0200

    One

---
+++ DataRawaylty/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataRawaylty.Controllers
+{
+    public class HomeController : Controller
+    {
+        string conn = @"Data Source=DESKTOP-2BGTDU9\SQLEXPRESS; User ID=DataRawaylty; Password=Awdsefdrgfth_1;";
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Register(string username, string password)
+        {
+            int message = 0;
+            using (SqlConnection sql = new SqlConnection(conn))
+            {
+                sql.Open();
+                SqlCommand cmd = new SqlCommand($"select * from [DataRawayltyDB].[dbo].[users] where [username] =@user", sql);
+                cmd.Parameters.AddWithValue("user", username.Trim());
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    message = 2;
+                }
+                else
+                {
+                    using(SqlConnection sql1 = new SqlConnection(conn))
+                    {
+                        sql1.Open();
+                        SqlCommand cmd1 = new SqlCommand($"insert into [DataRawayltyDB].[dbo].[users] (username, password) values (@user, @pass)", sql1);
+                        cmd1.Parameters.AddWithValue("user", username.Trim());
+                        cmd1.Parameters.AddWithValue("pass", password.Trim());
+                        int inserted = cmd1.ExecuteNonQuery();
+                        if(inserted > 0)
+                        {
+                            message = 1;
+                        }
+                    }
+                }
+
+
+            }
+                return Json(new { res = message }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult Login(string username, string password)
+        {
+            int message = 0;
+            using (SqlConnection sql = new SqlConnection(conn))
+            {
+                sql.Open();
+                SqlCommand cmd = new SqlCommand($"select * from [DataRawayltyDB].[dbo].[users] where [username] =@user and password=@pass", sql);
+                cmd.Parameters.AddWithValue("user", username.Trim());
+                cmd.Parameters.AddWithValue("pass", password.Trim());
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    message = 1;
+                }
+            }
+            return Json(new { res = message }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ DataRawaylty/Scripts/LoginRegister.js
@@ -0,0 +1,52 @@
+﻿$('#btn-login').click(function () {
+    let username = $('#input-loginuser').val();
+    let password = $('#input-loginpass').val();
+	if(username != '' && password != ''){
+		$.ajax({
+			url: '/Home/Login',
+			method: 'get',
+			data: {username: username, password: password},
+			success:function (result) {
+				if (result.res == 1) {
+					window.location.href = '/Home/Home';
+				} else {
+					$('#p-loginalert').css('visibility','visible');
+					$('#p-loginalert').text('Username or password is incorrect!');			
+						}
+			}   
+		})
+	} else {
+alert('Please fill up all fields!');
+	}
+})
+
+$('#btn-register').click(function () {
+	let username = $('#input-registeruser').val();
+	let password = $('#input-registerpass').val();
+
+	if(username != '' && password != ''){
+		$.ajax({
+			url: '/Home/Register',
+			method: 'post',
+			data: {username: username, password: password},
+			success:function (result) {
+				$('#p-registeralert').css('visibility','visible');
+				if (result.res == 1) {
+					$('#p-registeralert').text('Registration succesful!');
+					$('#input-registeruser,#input-registerpass').val('');
+				} else if (result.res == 2) {
+					$('#p-registeralert').text('Username already taken!');
+					$('#input-registeruser,#input-registerpass').val('');
+				} else {
+					alert('Something went wrong: (');	
+						}
+				}
+		})
+	} else {
+alert('Please fill up all fields!');
+	}
+
+	
+})
+
+
